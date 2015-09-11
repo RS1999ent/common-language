7593c161f746848bb3c463535a2ea3067c29da2e
David <tranlam@wisc.edu>
2015-09-11 07:48:58 -0500

    updated intradomain costs script, still not fast enough

--- simulator/scripts/computeIntraDomainCosts.py
+++ simulator/scripts/computeIntraDomainCosts.py
@@ -3,7 +3,7 @@ import Queue
 import random
 
 parser = argparse.ArgumentParser(description='Creates cache of intradomain costs for ASes')
-parser.add_argument('intraASinterPoP', metavar='intra as latencies', nargs = 1, help = 'intradomain latenceis for ases')
+parser.add_argument('intraASinterPoP', metavar='intra_as_latencies', nargs = 1, help = 'intradomain latenceis for ases')
 parser.add_argument('outFile', metavar='outputFile', nargs=1, help = 'file ot output this data')
 
 #open files based on arguments
@@ -13,44 +13,47 @@ outFile = open(args.outFile[0], 'w')
 
 #temporary hold for each lines adjacency list (call makeadjacency on each line)
 adjacencyList = {}
-asNum = ''
 #computes adjacency list for a line in the intradomain file
+#returns the asnumber
 def makeAdjacency(line):
     ASPops = line.split('|')
     asNum = ASPops[0]
     pops = ASPops[1].split(':')
-
     for element in pops:
         poppair = element.split(' ')
-        pop1 = poppair[0]
-        pop2 = poppair[1]
-        latency = poppair[2]
+        if poppair[0] != '\n':
+            pop1 = poppair[0]
+            pop2 = poppair[1]
+            latency = poppair[2]
 
-        if pop1 not in adjacencyList:
-            pairLatency = {}
-            adjacencyList[pop1] = pairLatency
-        if pop2 not in adjacencyList:
-            pairLatency = {}
-            adjacencyList[pop2] = pairLatency
+            if pop1 not in adjacencyList:
+                pairLatency = {}
+                adjacencyList[pop1] = pairLatency
+            if pop2 not in adjacencyList:
+                pairLatency = {}
+                adjacencyList[pop2] = pairLatency
 
-        adjacencyList[pop1][pop2] = latency
-        adjacencyList[pop2][pop1] = latency
+            adjacencyList[pop1][pop2] = latency
+            adjacencyList[pop2][pop1] = latency
+    return asNum
 
-#computes dijkstra between two pops
-def dijkstra (pop1, pop2):
+
+LARGEVAL = 9999
+#computes dijkstra between one and ever other pop
+def dijkstra (pop1):
     unsettledNodes = Queue.PriorityQueue()
     settledNodes = []
     distance = {}
 
     if pop1 not in adjacencyList:
         return 0
-    if pop2 not in adjacencyList:
-        return 0
+ #   if pop2 not in adjacencyList:
+  #      return 0
 
     for key in adjacencyList:
         if key != pop1:
-            distance[key] = 9999
-            unsettledNodes.put((9999, key))
+            distance[key] = LARGEVAL
+            unsettledNodes.put((LARGEVAL, key))
         
     distance[pop1] = 0
     unsettledNodes.put((0, pop1))
@@ -61,34 +64,36 @@ def dijkstra (pop1, pop2):
         if evalNode[1] not in settledNodes:
             settledNodes.append(evalNode[1])
         for key in neighbors:
-            if ket not in settledNodes:
-                potentialCost = neighbors[key] + evalNode[0]
+            if key not in settledNodes:
+                potentialCost = int(neighbors[key]) + evalNode[0]
                 if potentialCost < distance[key]:
                     distance[key] = potentialCost
                     unsettledNodes.put((potentialCost, key))
 
-    if distance[pop2] == 9999:
-        return 0
+   # if distance[pop2] == 9999:
+    #    return 0
 
-    return distance[pop2]
+    return distance
 
 #writes a line the file for poitns of presence, does dijsstra for each possible pair
-def writePoPLatencies(ASnum):
-    outLine = ASnum +'|'
-
+def writePoPLatencies(asNum):
+    print asNum, len(adjacencyList)
+    outLine = asNum +'|'
+    i = 0
     for node in adjacencyList:
-        for otherNode in adjacencyList:
-            if node != otherNode:
-                intradomainCost = dijkstra(node, otherNode)
-                outLine += str(node) + ' ' + str(otherNode) + ' ' + str(intradomainCost) + ':'
+       distances = dijkstra(node)
+       for key in distances:
+           if key != node:
+               outLine += str(node) + ' ' + str(key) + ' ' + str(distances[key]) + ':'
     outLine += '\n'
     outFile.write(outLine)
 
 
 
 for line in intraASFile:
-    makeAdjacency(line)
+    asNum = makeAdjacency(line)
     writePoPLatencies(asNum)
+    adjacencyList = {} #clear adjacency list
     
                                 
                             
