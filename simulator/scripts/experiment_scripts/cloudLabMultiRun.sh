bfa12d794928cfcb89a19192b0b5597d8819256d
tranlam <tranlam@node.tranlam-qv12070.netopt-pg0.wisc.cloudlab.us>
2015-12-21 14:06:00 -0600

    a run of results

--- simulator/scripts/experiment_scripts/cloudLabMultiRun.sh
+++ simulator/scripts/experiment_scripts/cloudLabMultiRun.sh
@@ -3,7 +3,7 @@ for j in $(seq 3 10);
 do
     test=$(echo "scale = 2; $j / 10" | bc)
     printf "\nTesting for stub/transit percentage: %f\n"  $test
-    for i in $(seq 1 5);
+    for i in $(seq 2 2);
     do
 	printf "\nTesting on Transit seed %s\n" $i
 	python ../asTypes.py --seedWiser 1 ../../formattedData/annotatedBrite.txt ../../formattedData/asTypes.txt --numTransits $test --seedTransit $i
