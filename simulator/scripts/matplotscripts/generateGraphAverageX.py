3eba5d0e6c0a770263c949082ad968895c282478
tranlam <tranlam@wisc.edu>
2016-01-14 14:08:14 -0600

    modified graph generator, added missing experiments

--- simulator/scripts/matplotscripts/generateGraphAverageX.py
+++ simulator/scripts/matplotscripts/generateGraphAverageX.py
@@ -7,43 +7,58 @@ import random
 import math
 import re
 
-DEBUG = 0
+DEBUG = 1
 
 parser = argparse.ArgumentParser(description='Creates a pdf graph from experimentoutput')
-parser.add_argument('experimentOutput', metavar='expOutput', nargs = 1, help = 'experimental output, relevent info should have GRAPH ')
+parser.add_argument('experimentOutput', metavar='expOutput', nargs ="+", help = 'experimental output, relevent info should have GRAPH and LEGEND ENDLEGEND')
 #parser.add_argument('pdfName', metavar='nameOfPdf', nargs=1, help = 'The name of the pdf')
 parser.add_argument('--xlabel', metavar='x axis label', help = 'label for the x axis')
 parser.add_argument('--ylabel', metavar='y axis label', help = 'label for the y axis')
 parser.add_argument('--title', metavar = 'graph title', help = 'title for the graph')
+parser.add_argument('--scale', metavar = 'scale y axis', help = 'value to scale down by', default=1)
 
 #open files based on arguments
 args = parser.parse_args()
-inputData = open(args.experimentOutput[0], 'r')
-pdfName = args.experimentOutput[0] + '.pdf'
+inputDataList = args.experimentOutput
+pdfName = args.title + '.pdf'
 xlabel = args.xlabel
 ylabel = args.ylabel
 title = args.title
-xyDict = {}
+scalingFactor = args.scale
+rawData = [] #list of tuples (xydictionary, legendname)
 X = []
 Y = []
 ymax = 0
 
 def parseInput():
-    p = re.compile('GRAPH.*ENDGRAPH')
-    for line in inputData:
-        if p.search(line):
-            m = p.search(line)
-            line = m.group()
-            splitLine = line.split()
-            x = splitLine[1]
-            y = splitLine[2]
-            if DEBUG:
-                print x,y
-            if x not in xyDict:
-                xyDict[x] = []
-            xyDict[x].append(y)
-    if DEBUG:
-        print "xyDict: ", xyDict
+    for entry in inputDataList:
+        expOutput = open(entry, 'r')
+        p = re.compile('GRAPH.*ENDGRAPH')
+        legendRE = re.compile('LEGEND.*ENDLEGEND')
+        legendName = ''
+        xyDict = {}
+        for line in expOutput:
+            if p.search(line):
+                m = p.search(line)
+                line = m.group()
+                splitLine = line.split()
+                x = splitLine[1]
+                y = splitLine[2]
+                if DEBUG:
+                    print x,y
+                if x not in xyDict:
+                    xyDict[x] = []
+                xyDict[x].append(y)
+                
+            if legendRE.search(line):
+                m = legendRE.search(line)
+                line = m.group()
+                splitLine = line.split()
+                splitLine = splitLine[1:-1]
+                legendName = legendName.join(splitLine)
+        rawData.append((xyDict, legendName))
+        if DEBUG:
+            print rawData
 
 #computes the average from a list of floats
 def getAverage(list):
@@ -52,30 +67,38 @@ def getAverage(list):
         sum += float(item)
     average = sum/len(list)
     if DEBUG:
+        print "sum: ", sum
+        print "listlen: ", len(list)
         print "Average: ",average
     return average
-        
-def fillXY():
+
+#return list of y values
+#input dictionary of xyvalues
+def getY(xyDict):
+    y = []
     for key in sorted(xyDict):
         xAverage = getAverage(xyDict[key])
-        global ymax
-        if xAverage > ymax:
-            ymax = xAverage
-        X.append(key)
-        Y.append(xAverage)
+        y.append(xAverage/float(scalingFactor))
     if DEBUG:
-        print "x: ", X
-        print "y: ", Y
-        
+        print "y: ", y
+    return y
+
+#get x values associated with this dictionary
+def getX(xyDict):
+    x = []
+    for key in sorted(xyDict):
+        x.append(key)
+    if DEBUG:
+        print 'x: ', x
+    return x
 
 parseInput()
-fillXY()
+#fillXY()
         
 pp = PdfPages(pdfName)
 arr = numpy.asarray
 
-x = arr(X)
-y = arr(Y)
+#y = arr(Y)
 #y = arr([[1457270.5],
 #         [1479158.9],
 #         [1476073.413],
@@ -102,14 +125,27 @@ y = arr(Y)
 
 #plt.figure()
 #plt.errorbar(x, y, yerr=0)
-plt.plot(x,y)
+legendHandles = []
+for tuple in rawData:
+    xyDict = tuple[0]
+    legendName = tuple[1]
+    if DEBUG:
+        print 'legendname: ', legendName
+    y = getY(xyDict)
+    x = getX(xyDict)
+    handle, =plt.plot(arr(x),arr(y), label=legendName)
+    legendHandles.append(handle)
+
+plt.legend(fontsize='small', loc=2)
+    
 #plt.axis([0,1, 0, 1520000])
 plt.ylabel(ylabel)
 plt.xlabel(xlabel)
-plt.title(title)
+plt.xticks(numpy.arange(0, 1.1, .1))
+pylab.ylim(ymin=0)
 pp.savefig()
 pp.close()
-#plt.show()
+plt.show()
 
 #pylab.errorbar(x, y, yerr=c)
 #pylab.show()
