1920e8414ac19404c01df3b18a7b155c4a7a1b01
tranlam <tranlam@node.tranlam-qv12663.netopt-pg0.wisc.cloudlab.us>
2016-01-26 05:23:08 -0600

    need to run these experts faster

--- simulator/scripts/experiment_scripts/runExpScratchFileLinux.sh
+++ simulator/scripts/experiment_scripts/runExpScratchFileLinux.sh
@@ -16,16 +16,16 @@ python ../richWorldASTypes.py --seedWiser 1 ../../formattedData/annotatedBrite.t
 java -Xmx10000M -Dfile.encoding=Cp1252 -cp .:../../src:../../../deps/argparse4j-0.6.0.jar  simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $3 --useBandwidth $4 --forX 0 --metric $5 --maxPaths $6
 	java -Xmx1500M -Dfile.encoding=Cp1252 -classpath "C:\cygwin64\home\David\repos\commonlanguage\simulator\src;C:\Users\spart\.m2\repository\net\sourceforge\argparse4j\argparse4j\0.6.0\argparse4j-0.6.0.jar;C:\Users\spart\.m2\repository" simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $3 --useBandwidth $4 --forX 0 --metric $5 --maxPaths $6
 
-for j in $(seq .5 .5 9.5);
+for j in $(seq .1 .1 .9);
 do
     test=$(echo "scale = 2; $j / 10" | bc)
     printf "\nTesting for stub/transit percentage: %f\n"  $test
     for i in $(seq 1 10);
     do
 	printf "\nTesting on Transit seed %s\n" $i
-	python ../richWorldASTypes.py --seedWiser 1 ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" --numTransits $test --seedTransit $i --sim $1 --randomMethod $7
-	java -Xmx10000M -Dfile.encoding=Cp1252 -cp .:../../src:../../../deps/argparse4j-0.6.0.jar  simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $3 --useBandwidth $4 --forX $test --metric $5 --maxPaths $6
-	java -Xmx1500M -Dfile.encoding=Cp1252 -classpath "C:\cygwin64\home\David\repos\commonlanguage\simulator\src;C:\Users\spart\.m2\repository\net\sourceforge\argparse4j\argparse4j\0.6.0\argparse4j-0.6.0.jar;C:\Users\spart\.m2\repository" simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $3 --useBandwidth $4 --forX $test --metric $5 --maxPaths $6
+	python ../richWorldASTypes.py --seedWiser 1 ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" --numTransits $j --seedTransit $i --sim $1 --randomMethod $7
+	java -Xmx10000M -Dfile.encoding=Cp1252 -cp .:../../src:../../../deps/argparse4j-0.6.0.jar  simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $3 --useBandwidth $4 --forX $j --metric $5 --maxPaths $6
+	java -Xmx1500M -Dfile.encoding=Cp1252 -classpath "C:\cygwin64\home\David\repos\commonlanguage\simulator\src;C:\Users\spart\.m2\repository\net\sourceforge\argparse4j\argparse4j\0.6.0\argparse4j-0.6.0.jar;C:\Users\spart\.m2\repository" simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $3 --useBandwidth $4 --forX $j --metric $5 --maxPaths $6
     done
 done
 
