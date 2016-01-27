c367711eb2a8f056ee7b15772884f8bcf542bc55
tranlam <tranlam@node.tranlam-qv12663.netopt-pg0.wisc.cloudlab.us>
2016-01-26 21:56:14 -0600

    modified so 9th argument gives richworld to astyupes

--- simulator/scripts/experiment_scripts/runExpScratchFileLinux.sh
+++ simulator/scripts/experiment_scripts/runExpScratchFileLinux.sh
@@ -8,27 +8,28 @@
 #echo arg6 is max number of propagated paths
 echo "arg7 is the random method, 0 for pool, 1 for weigthed, 2 for uniform"
 #echo arg8 is the legend name
+echo "arg9 is whether to perfomr richworld"
 astypesFile=$*astypes.txt
 echo $astypesFile
 echo "LEGEND "$8" ENDLEGEND\n"
 
-python ../richWorldASTypes.py --seedWiser 1 ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" --numTransits 0 --seedTransit 1 --sim $1 --randomMethod $7
+python ../richWorldASTypes.py --seedWiser 1 ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" --numTransits 0 --seedTransit 1 --sim $1 --randomMethod $7 --richworld $9
 java -Xmx10000M -Dfile.encoding=Cp1252 -cp .:../../src:../../../deps/argparse4j-0.6.0.jar  simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $3 --useBandwidth $4 --forX 0 --metric $5 --maxPaths $6
 	java -Xmx1500M -Dfile.encoding=Cp1252 -classpath "C:\cygwin64\home\David\repos\commonlanguage\simulator\src;C:\Users\spart\.m2\repository\net\sourceforge\argparse4j\argparse4j\0.6.0\argparse4j-0.6.0.jar;C:\Users\spart\.m2\repository" simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $3 --useBandwidth $4 --forX 0 --metric $5 --maxPaths $6
 
-for j in $(seq .1 .5 .95);
+for j in $(seq .1 .1 .9);
 do
     test=$(echo "scale = 2; $j / 10" | bc)
     printf "\nTesting for stub/transit percentage: %f\n"  $test
     for i in $(seq 1 10);
     do
 	printf "\nTesting on Transit seed %s\n" $i
-	python ../richWorldASTypes.py --seedWiser 1 ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" --numTransits $j --seedTransit $i --sim $1 --randomMethod $7
+	python ../richWorldASTypes.py --seedWiser 1 ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" --numTransits $j --seedTransit $i --sim $1 --randomMethod $7 --richworld $9
 	java -Xmx10000M -Dfile.encoding=Cp1252 -cp .:../../src:../../../deps/argparse4j-0.6.0.jar  simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $3 --useBandwidth $4 --forX $j --metric $5 --maxPaths $6
 	java -Xmx1500M -Dfile.encoding=Cp1252 -classpath "C:\cygwin64\home\David\repos\commonlanguage\simulator\src;C:\Users\spart\.m2\repository\net\sourceforge\argparse4j\argparse4j\0.6.0\argparse4j-0.6.0.jar;C:\Users\spart\.m2\repository" simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $3 --useBandwidth $4 --forX $j --metric $5 --maxPaths $6
     done
 done
 
-python ../richWorldASTypes.py --seedWiser 1 ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" --numTransits 1 --seedTransit 1 --sim $1 --randomMethod $7
+python ../richWorldASTypes.py --seedWiser 1 ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" --numTransits 1 --seedTransit 1 --sim $1 --randomMethod $7 --richworld $9
 java -Xmx10000M -Dfile.encoding=Cp1252 -cp .:../../src:../../../deps/argparse4j-0.6.0.jar  simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $3 --useBandwidth $4 --forX 1.000 --metric $5 --maxPaths $6
 	java -Xmx1500M -Dfile.encoding=Cp1252 -classpath "C:\cygwin64\home\David\repos\commonlanguage\simulator\src;C:\Users\spart\.m2\repository\net\sourceforge\argparse4j\argparse4j\0.6.0\argparse4j-0.6.0.jar;C:\Users\spart\.m2\repository" simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $3 --useBandwidth $4 --forX 1.000 --metric $5 --maxPaths $6
