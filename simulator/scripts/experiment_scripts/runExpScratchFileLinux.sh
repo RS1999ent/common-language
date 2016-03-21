2235bd0e7e89af2f47fe6eeadfb85f22aaa3a8e8
tranlam <tranlam@wisc.edu>
2016-03-21 10:11:47 -0500

    created updated readme and added extra dependency in runexpscratchfilelinux

--- simulator/scripts/experiment_scripts/runExpScratchFileLinux.sh
+++ simulator/scripts/experiment_scripts/runExpScratchFileLinux.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 echo "arg1 is the astypes to generate, 501-wiser, 504 sbgp, 505 bw 507 replacement"
-echo "arg2 is the sim to perform in the simulator; 4 replacement, 5 truecost"
-echo "arg3 is where to monitor in the simulator, participating 0, all 1, gulf 2"
-echo "arg4 is whether to use bandwidth numbers in the simulator"
-echo "arg5 is what metric to use RIBMETRIC 0, FIBMETRIC 1"
-echo "arg6 is max number of propagated paths"
-echo "arg7 is the random method, 0 for pool, 1 for weigthed, 2 for uniform"
-echo "arg8 is the legend name"
-echo "arg9 is whether to perfomr richworld"
+echo "arg2 is where to monitor in the simulator, participating 0, all 1, gulf 2"
+echo "arg3 is max number of propagated paths"
+echo "arg4 is the random method, 0 for pool, 1 for weigthed, 2 for uniform"
+echo "arg5 is the legend name"
+echo "arg6 is whether to perfomr richworld"
 astypesFile=$*astypes.txt
 echo $astypesFile
-echo "LEGEND "$8" ENDLEGEND\n"
+echo "LEGEND "$5" ENDLEGEND\n"
 
-#python ../richWorldASTypes.py --seedWiser 1 ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" --numTransits 0 --seedTransit 1 --sim $1 --randomMethod $7 --richworld $9
-#java -Xmx10000M -Dfile.encoding=Cp1252 -cp .:../../src:../../../deps/argparse4j-0.6.0.jar  simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $3 --useBandwidth $4 --forX 0 --metric $5 --maxPaths $6
-#	java -Xmx1500M -Dfile.encoding=Cp1252 -classpath "C:\cygwin64\home\David\repos\commonlanguage\simulator\src;C:\Users\spart\.m2\repository\net\sourceforge\argparse4j\argparse4j\0.6.0\argparse4j-0.6.0.jar;C:\Users\spart\.m2\repository" simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $3 --useBandwidth $4 --forX 0 --metric $5 --maxPaths $6
+#python ../richWorldASTypes.py --seedWiser 1 ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" --numTransits 0 --seedTransit 1 --sim $1 --randomMethod $4 --richworld $6
+#java -Xmx10000M -Dfile.encoding=Cp1252 -cp .:../../src:../../../deps/argparse4j-0.6.0.jar  simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $2 --useBandwidth $4 --forX 0 --metric $5 --maxPaths $3
+#	java -Xmx1500M -Dfile.encoding=Cp1252 -classpath "C:\cygwin64\home\David\repos\commonlanguage\simulator\src;C:\Users\spart\.m2\repository\net\sourceforge\argparse4j\argparse4j\0.6.0\argparse4j-0.6.0.jar;C:\Users\spart\.m2\repository" simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $2 --useBandwidth $4 --forX 0 --metric $5 --maxPaths $3
 
 for j in $(seq .1 .1 .9);
 do
@@ -24,12 +21,12 @@ do
     for i in $(seq 1 10);
     do
 	printf "\nTesting on Transit seed %s\n" $i
-	python ../richWorldASTypes.py --seedWiser 1 ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" --numTransits $j --seedTransit $i --sim $1 --randomMethod $7 --richworld $9
-	java -Xmx10000M -Dfile.encoding=Cp1252 -cp .:../../src:../../../deps/argparse4j-0.6.0.jar  simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $3 --useBandwidth $4 --forX $j --metric $5 --maxPaths $6
-	java -Xmx1500M -Dfile.encoding=Cp1252 -classpath "C:\cygwin64\home\David\repos\commonlanguage\simulator\src;C:\Users\spart\.m2\repository\net\sourceforge\argparse4j\argparse4j\0.6.0\argparse4j-0.6.0.jar;C:\Users\spart\.m2\repository" simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $3 --useBandwidth $4 --forX $j --metric $5 --maxPaths $6
+	python ../richWorldASTypes.py --seedWiser 1 ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" --numTransits $j --seedTransit $i --sim $1 --randomMethod $4 --richworld $6
+	java -Xmx10000M -Dfile.encoding=Cp1252 -cp .:../../src:../../../deps/argparse4j-0.6.0.jar:../../src:../../../deps/snakeyaml-1.17.jar  simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim 3 --monitorFrom $2 --forX $j  --maxPaths $3
+	java -Xmx1500M -Dfile.encoding=Cp1252 -classpath "C:\cygwin64\home\David\repos\commonlanguage\simulator\src;C:\Users\spart\.m2\repository\net\sourceforge\argparse4j\argparse4j\0.6.0\argparse4j-0.6.0.jar;C:\Users\spart\.m2\repository;C:\cygwin64\home\David\repos\commonlanguage\deps\snakeyaml-1.17.jar" simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim 3 --monitorFrom $2 --forX $j  --maxPaths $3
     done
 done
 
-python ../richWorldASTypes.py --seedWiser 1 ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" --numTransits 1 --seedTransit 1 --sim $1 --randomMethod $7 --richworld $9
-java -Xmx10000M -Dfile.encoding=Cp1252 -cp .:../../src:../../../deps/argparse4j-0.6.0.jar  simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $3 --useBandwidth $4 --forX 1.000 --metric $5 --maxPaths $6
-	java -Xmx1500M -Dfile.encoding=Cp1252 -classpath "C:\cygwin64\home\David\repos\commonlanguage\simulator\src;C:\Users\spart\.m2\repository\net\sourceforge\argparse4j\argparse4j\0.6.0\argparse4j-0.6.0.jar;C:\Users\spart\.m2\repository" simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim $2 --monitorFrom $3 --useBandwidth $4 --forX 1.000 --metric $5 --maxPaths $6
+python ../richWorldASTypes.py --seedWiser 1 ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" --numTransits 1 --seedTransit 1 --sim $1 --randomMethod $4 --richworld $6
+java -Xmx10000M -Dfile.encoding=Cp1252 -cp .:../../src:../../../deps/argparse4j-0.6.0.jar:../../src:../../../deps/snakeyaml-1.17.jar  simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim 3 --monitorFrom $2  --forX 1.000 --maxPaths $3
+	java -Xmx1500M -Dfile.encoding=Cp1252 -classpath "C:\cygwin64\home\David\repos\commonlanguage\simulator\src;C:\Users\spart\.m2\repository\net\sourceforge\argparse4j\argparse4j\0.6.0\argparse4j-0.6.0.jar;C:\Users\spart\.m2\repository;C:\cygwin64\home\David\repos\commonlanguage\deps\snakeyaml-1.17.jar" simulator.Simulator ../../formattedData/annotatedBrite.txt ../../formattedData/"$astypesFile" ../../results/delete.txt --seed 1 --sim 3 --monitorFrom $2 --forX 1.000 --maxPaths $3
